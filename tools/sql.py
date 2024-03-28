010e8b15c10e3ad22d626e22b9c86afbf4c3b7ad
levblanc <levblanc@gmail.com>
2024-03-28 14:50:17 +0800

    feat: catch SQL query error and return string mesasge

--- tools/sql.py
+++ tools/sql.py
@@ -6,8 +6,11 @@ conn = sqlite3.connect("db.sqlite")
 
 def run_sqlite_query(query):
     cursor = conn.cursor()
-    cursor.execute(query)
-    return cursor.fetchall()
+    try:
+        cursor.execute(query)
+        return cursor.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occured: {str(err)}"
 
 
 run_query_tool = Tool.from_function(
